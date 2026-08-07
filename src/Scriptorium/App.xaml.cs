ea6a629eee834ecf51c6fa7cb026325a7ad2146e
Paul Muntean <paulmunteanst@gmail.com>
2026-08-07 03:19:57 +0300

    Create initial WPF solution

---
+++ src/Scriptorium/App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace Scriptorium;
+
+public partial class App : Application
+{
+}
---
+++ src/Scriptorium/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Scriptorium;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
